f4f6c9719c5ab48a47daa2f372ab8dc28cbe1ff1
Glitch (giphyproject) <none>
2021-04-22 22:37:27 +0000

    🚙⚽️ Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -3,9 +3,30 @@
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 /* global $ */
 
+
+
 $(".search-button").click(function(){
+   let userInput = $(".search-term").val()
+   let mainUrl =   `https://api.giphy.com/v1/gifs/search?q='${userInput}'&rating=pg&api_key=dc6zaTOxFJmzC`;
+   
+  fetch(mainUrl)
+    .then(function(response) {
+    return response.json();
+    
+})
+    .then(function(data) {
+    // console.log(data.data[0].images.original.url)
+    let imgUrl = data.data[0].images.original.url;
+    let imgEl = '<img src=' + imgUrl + '>';
+    $('.main').append(imgEl);
+  
+});
+ 
+  
+
   
 
   
 });
+
 
